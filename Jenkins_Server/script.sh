ed9b4ec64233443d3b2ee8a56a6addf343bd42f4
OGUNLEYE AYO DAMOLA <104855549+ogunleye0720@users.noreply.github.com>
2023-05-07 21:56:55 +0100

    Update script.sh

--- Jenkins_Server/script.sh
+++ Jenkins_Server/script.sh
@@ -67,10 +67,6 @@ sudo apt-get update
 
 sudo apt install openjdk-11-jre -y
 
-java -version
-
-sudo su
-
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
@@ -87,14 +83,11 @@ sudo systemctl enable jenkins
 
 sudo systemctl start jenkins
 
-sudo systemctl status jenkins
-
 sudo ufw allow OpenSSH 
 
 sudo ufw enable
 
 sudo ufw allow 8080
 
-sudo ufw status
 
 
